9abd1abc423e1343188e9a81b14c3bfdb7ff4301
Simon Buchli, ICT Berufsbildungscenter AG, simon.buchli@bbcag.ch <Simon Buchli, ICT Berufsbildungscenter AG, simon.buchli@bbcag.ch@ws-zbuchs.bbcnet-zh.local>
2015-05-13 12:33:51 +0200

    ERSTER COMMIT

---
+++ Adventure-of-Kira-GUI-Overworld/src/backup/helloworld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package backup;
+
+/**
+ * @author Simon Buchli, ICT Berufsbildungscenter AG, simon.buchli@bbcag.ch
+ *
+ */
+public class helloworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("HEllo World");
+	}
+
+}
